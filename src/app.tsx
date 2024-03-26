06c5039c1596a62a2bcb3eacd488935c39c29dc4
guy-jerome <aaronjamesroberts@gmail.com>
2024-03-26 15:04:55 -0400

    Added the counter logic and styling

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,62 @@
 import { useState } from "preact/hooks";
-
+import "./app.css";
 export function App() {
-  return <></>;
+  const [counter, setCounter] = useState(0);
+  const [amount, setAmount] = useState(1);
+
+  function handleIncrease() {
+    setCounter(counter + amount);
+  }
+
+  function handleDecrease() {
+    setCounter(counter - amount);
+  }
+
+  function handleAmountChange(e: any) {
+    const value = e.target.value;
+
+    if (/^\d*$/.test(value) && !isNaN(value)) {
+      setAmount(parseInt(value, 10));
+    }
+  }
+
+  function handleKeyPress(e: any) {
+    // Allow only numbers
+    const isValidInput = /^\d*$/.test(e.key);
+    if (!isValidInput) {
+      e.preventDefault();
+    }
+  }
+
+  function handleReset() {
+    setCounter(0);
+  }
+
+  return (
+    <>
+      <h1>Counter</h1>
+      <main>
+        <div>{counter}</div>
+        <div>
+          <button className="change-btn" onClick={handleIncrease}>
+            +
+          </button>
+          <button className="change-btn" onClick={handleDecrease}>
+            -
+          </button>
+        </div>
+        <div>
+          <label htmlFor="valueInput">Increment/Decrement by: </label>
+          <input
+            id="valueInput"
+            type="text"
+            value={amount}
+            onChange={handleAmountChange}
+            onKeyPress={handleKeyPress}
+          />
+        </div>
+        <button onClick={handleReset}>Reset</button>
+      </main>
+    </>
+  );
 }
